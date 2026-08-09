b4a8447f2925628622f639896c05d646a9c95ab5
Asep Saepudin <saepudinasep2001@gmail.com>
2026-08-09 13:40:34 +0700

    [ADD] Menambahkan configurasi env

--- server/database/database.go
+++ server/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/saepudinasep/blog-go-react/server/model"
 	"gorm.io/driver/mysql"
@@ -12,10 +13,15 @@ import (
 var DBConn *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:@tcp(localhost:3306)/fiber_blog?charset=utf8mb4&parseTime=True&loc=Local"
+
+	user := os.Getenv("db_user")
+	password := os.Getenv("db_password")
+	dbName := os.Getenv("db_name")
+
+	dsn := user + ":" + password + "@tcp(localhost:3306)/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Warn),
+		Logger: logger.Default.LogMode(logger.Error),
 	})
 
 	if err != nil {
--- server/server.go
+++ server/server.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 	"github.com/saepudinasep/blog-go-react/server/database"
 	"github.com/saepudinasep/blog-go-react/server/router"
 )
 
 func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	database.ConnectDB()
 }
 
