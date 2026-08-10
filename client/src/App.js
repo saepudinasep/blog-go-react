087ca985c6075d5d1fccb7bd93bcc6755a987859
Asep Saepudin <saepudinasep2001@gmail.com>
2026-08-10 20:47:06 +0700

    [ADD] Menambahkan template react js + bootstrap + axios

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col xs='12 py-2'>
+          <h1 className='text-center mt-5'>React Application with Go Fiber Backend</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
