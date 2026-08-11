64c3b5bf8a777868db56d910fec87407a9a25a26
Asep Saepudin <saepudinasep2001@gmail.com>
2026-08-11 10:07:28 +0700

    [ADD] Connect server tp client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,47 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiUrl = process.env.REACT_APP_API_ROOT + 'blogs';
+
+        const response = await axios.get(apiUrl);
+
+        if (response.status === 200) {
+          if (response?.data.statusText === 'OK') {
+            setApiData(response?.data?.blog_records);
+          }
+        }
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(apiData);
+
   return (
     <Container>
       <Row>
-        <Col xs='12 py-2'>
+        <Col xs='12' className='py-2'>
           <h1 className='text-center mt-5'>React Application with Go Fiber Backend</h1>
         </Col>
+
+        {apiData &&
+          apiData.map((record, index) => (
+            <Col xs='4' className='py-5 box' key={index}>
+              <div className='title'>{record.title}</div>
+              <div>{record.post}</div>
+            </Col>
+          ))}
       </Row>
     </Container>
   );
--- server/server.go
+++ server/server.go
@@ -4,10 +4,11 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"github.com/saepudinasep/blog-go-react/server/database"
 	"github.com/saepudinasep/blog-go-react/server/router"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func init() {
@@ -28,7 +29,10 @@ func main() {
 
 	app := fiber.New()
 
-	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 
